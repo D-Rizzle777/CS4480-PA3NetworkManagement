2f40d100a3ca984384c187a01d248d4a15c5e7f8
D-Rizzle777 <122405189+D-Rizzle777@users.noreply.github.com>
2025-04-14 05:04:33 -0600

    Initial upload

---
+++ cs4480-pa3/part1/r1-ospf-config.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Configure OSPF on R1
+docker exec -it r1 vtysh -c 'configure terminal' -c 'router ospf' -c 'ospf router-id 1.1.1.1' -c 'network 10.0.1.0/24 area 0.0.0.0' -c 'network 10.0.12.0/24 area 0.0.0.0' -c 'network 10.0.14.0/24 area 0.0.0.0' -c 'exit' -c 'exit'
+
+# Set initial interface costs
+docker exec -it r1 vtysh -c 'configure terminal' -c 'interface eth0' -c 'ip ospf cost 10' -c 'exit' -c 'interface eth1' -c 'ip ospf cost 10' -c 'exit' -c 'interface eth2' -c 'ip ospf cost 20' -c 'exit' -c 'exit'
+
+# Save the configuration
+docker exec -it r1 vtysh -c 'write memory'
+
+# Show the configuration
+docker exec -it r1 vtysh -c 'show running-config'
