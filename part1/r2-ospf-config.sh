a48c7cf7c91790ef288250dec115a9ae82b0a807
D-Rizzle777 <122405189+D-Rizzle777@users.noreply.github.com>
2025-04-14 07:05:01 -0600

    Update r2-ospf-config.sh

--- part1/r2-ospf-config.sh
+++ part1/r2-ospf-config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Configure OSPF on R2
-docker exec -it r2 vtysh -c 'configure terminal' -c 'router ospf' -c 'ospf router-id 2.2.2.2' -c 'network 10.0.12.0/24 area 0.0.0.0' -c 'network 10.0.23.0/24 area 0.0.0.0' -c 'exit' -c 'exit'
+docker exec -it r2 vtysh -c 'configure terminal' -c 'router ospf' -c 'ospf router-id 2.2.2.2' -c 'network 10.0.10.0/24 area 0.0.0.0' -c 'network 10.0.11.0/24 area 0.0.0.0' -c 'exit' -c 'exit'
 
 # Set initial interface costs
 docker exec -it r2 vtysh -c 'configure terminal' -c 'interface eth0' -c 'ip ospf cost 10' -c 'exit' -c 'interface eth1' -c 'ip ospf cost 10' -c 'exit' -c 'exit'
