253a949e8b5f8f8cd6544000212182f7d4641a83
D-Rizzle777 <danielbro33@gmail.com>
2025-04-15 00:19:55 -0600

    Should be working now maybe bro

--- part1/configure-host-routes.sh
+++ part1/configure-host-routes.sh
@@ -2,11 +2,13 @@
 
 # Configure routes on HostA
 echo "Configuring routes on HostA..."
+docker exec -it ha ip route del default
 # Add a default route via R1
 docker exec -it ha ip route add default via 10.0.14.4
 
 # Configure routes on HostB
 echo "Configuring routes on HostB..."
+docker exec -it hb ip route del default
 # Add a default route via R3
 docker exec -it hb ip route add default via 10.0.15.4
 
